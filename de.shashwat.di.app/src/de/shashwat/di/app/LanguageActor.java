cbb5517bb03a6b80dda65968997645a886da41f2
shashwata <shashwat.anand@gmail.com>
2018-04-14 20:34:48 +0530

    Adding language actor code

--- de.shashwat.di.app/src/de/shashwat/di/app/LanguageActor.java
+++ de.shashwat.di.app/src/de/shashwat/di/app/LanguageActor.java
@@ -1,5 +1,37 @@
 package de.shashwat.di.app;
 
+import javax.inject.Inject;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+
+import de.shashwat.di.services.ILanguageProviderService;
+
+/**
+ * The Class LanguageActor.
+ */
 public class LanguageActor {
+	
+	/** The language service. */
+	private final ILanguageProviderService languageService;
+	
+	/**
+	 * Instantiates a new language actor.
+	 *
+	 * @param languageService the language service
+	 */
+	@Inject
+	public LanguageActor(final ILanguageProviderService languageService) {
+		this.languageService = languageService;
+	}
+	
+	/**
+	 * Gets the request language.
+	 *
+	 * @return the request language
+	 */
+	@Execute
+	public String getRequestLanguage() {
+		return this.languageService.getLanguage();
+	}
 
 }
