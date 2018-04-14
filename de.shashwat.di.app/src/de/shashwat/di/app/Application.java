f4ce86a19798c7231e39d90a56f66f30a8bbbeac
shashwata <shashwat.anand@gmail.com>
2018-04-14 20:50:21 +0530

    Adding Greeting actor class

--- de.shashwat.di.app/src/de/shashwat/di/app/Application.java
+++ de.shashwat.di.app/src/de/shashwat/di/app/Application.java
@@ -1,5 +1,28 @@
 package de.shashwat.di.app;
 
-public class Application {
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class Application.
+ */
+public class Application implements IApplication {
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.equinox.app.IApplication#start(org.eclipse.equinox.app.IApplicationContext)
+	 */
+	@Override
+	public Object start(IApplicationContext context) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.eclipse.equinox.app.IApplication#stop()
+	 */
+	@Override
+	public void stop() {
+		// Nothing
+	}
 }
--- de.shashwat.di.app/src/de/shashwat/di/app/GreetingActor.java
+++ de.shashwat.di.app/src/de/shashwat/di/app/GreetingActor.java
@@ -1,5 +1,63 @@
 package de.shashwat.di.app;
 
-public class GreetingActor {
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import javax.inject.Named;
 
+import de.shashwat.di.services.IGreetingService;
+import de.shashwat.di.services.ITranslationService;
+
+/**
+ * The Class GreetingActor.
+ */
+public class GreetingActor {
+	
+	/** The person name. */
+	private final String personName;
+	
+	/** The greeting service. */
+	private IGreetingService greetingService;
+	
+	/** The language. */
+	private String language;
+	
+	/**
+	 * Instantiates a new greeting actor.
+	 *
+	 * @param personName the person name
+	 */
+	@Inject
+	public GreetingActor(@Named("Name") final String personName) {
+		this.personName = personName;
+	}
+	
+	/**
+	 * Inits the.
+	 *
+	 * @param greetingService the greeting service
+	 */
+	@PostConstruct
+	private void init(final IGreetingService greetingService) {
+		this.greetingService = greetingService;
+	}
+	
+	/**
+	 * Sets the language.
+	 *
+	 * @param language the new language
+	 */
+	@Inject
+	public void setLanguage(@Named("Language") final String language) {
+		this.language = language;
+	}
+	
+	/**
+	 * Greet.
+	 *
+	 * @param translationService the translation service
+	 */
+	public void greet(ITranslationService translationService) {
+		this.greetingService.greet(translationService.translate(this.language, 
+				"Hello")[0] + " ' " + this.personName + " '!");
+	}
 }
