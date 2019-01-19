f1dbd7222e283c632067ff422a7aadd8a726bcd3
shashwatanand <shashwat.anand@gmail.com>
2019-01-19 17:30:06 +0100

    Updating project

--- vert.x_reactivejx/vertex.reactivejx.sample/src/main/java/de/shashwat/vertex/reactivejx/sample/SampleAppMain.java
+++ vert.x_reactivejx/vertex.reactivejx.sample/src/main/java/de/shashwat/vertex/reactivejx/sample/SampleAppMain.java
@@ -1,7 +1,24 @@
 package de.shashwat.vertex.reactivejx.sample;
 
+import io.vertx.ext.web.Router;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
+import io.vertx.core.http.HttpServerResponse;
+
 public class SampleAppMain {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		Vertx vertx = Vertx.factory.vertx();
+		
+		// creating server using vertx
+		HttpServer server = vertx.createHttpServer();
+		
+		// creating router
+		Router router = Router.router(vertx);
+		
+		router.route().handler(rContext -> {
+			HttpServerResponse response = rContext.response();
+			response.setChunked(true).write("Hello from Sample App").end();
+		});
+		server.requestHandler(router::accept).listen(8081);
 	}
 }
