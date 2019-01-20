b1a39b318c3b0d938595c1cea5b5d3cdda1f9004
shashwatanand <shashwat.anand@gmail.com>
2019-01-20 11:28:05 +0100

    initial commit

---
+++ rxjava_eclipse/de.shashwat.eclipse.rxjava.sample/src/de/shashwat/eclipse/rxjava/sample/service/ui/RxPart.java
@@ -0,0 +1,19 @@
+
+package de.shashwat.eclipse.rxjava.sample.service.ui;
+
+import javax.inject.Inject;
+import javax.annotation.PostConstruct;
+import org.eclipse.swt.widgets.Composite;
+
+public class RxPart {
+	@Inject
+	public RxPart() {
+
+	}
+
+	@PostConstruct
+	public void postConstruct(Composite parent) {
+
+	}
+
+}
