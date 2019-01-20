cd0363ae90088217c944f9a47db02e0811af1f21
shashwatanand <shashwat.anand@gmail.com>
2019-01-20 11:24:19 +0100

    Adding impl of Service

--- rxjava_eclipse/de.shashwat.eclipse.rxjava.sample/src/de/shashwat/eclipse/rxjava/sample/service/StudentServiceImpl.java
+++ rxjava_eclipse/de.shashwat.eclipse.rxjava.sample/src/de/shashwat/eclipse/rxjava/sample/service/StudentServiceImpl.java
@@ -1,6 +1,11 @@
 package de.shashwat.eclipse.rxjava.sample.service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+import org.eclipse.core.runtime.jobs.Job;
 
 import io.reactivex.Maybe;
 import io.reactivex.Single;
@@ -9,14 +14,45 @@ import io.reactivex.Single;
  * The Class StudentServiceImpl.
  */
 public class StudentServiceImpl implements IStudentService {
+	private List<StudentModel> students;
+
+	public StudentServiceImpl() {
+		this.students = createStudents();
+	}
+	
+	private List<StudentModel> createStudents() {
+		List<StudentModel> returnList = new ArrayList<>();
+		
+		returnList.add(createStudent("1", "Shashwat", "CSE", "MIT"));
+		returnList.add(createStudent("2", "Shubhangi", "MBA", "Manage"));
+		returnList.add(createStudent("3", "Manish", "CSE", "BCET"));
+		returnList.add(createStudent("4", "Santosh", "CSE", "UVCE"));
+		returnList.add(createStudent("5", "Rajan", "ECE", "BCET"));
+		returnList.add(createStudent("6", "Nikesh", "ECE", "BCET"));
+		returnList.add(createStudent("7", "Sanjay", "ECE", "BCET"));
+		returnList.add(createStudent("8", "Vinay", "CSE", "MIT"));
+		returnList.add(createStudent("9", "Shradha", "MCA", "PuneU"));
+		returnList.add(createStudent("10", "Shivshankar", "CSE", "MIT"));
+		return returnList;
+	}
+
+	private StudentModel createStudent(String id, String name, String department, String collegeName) {
+		return new StudentModel(id, name, department, collegeName);
+	}
 
 	/* (non-Javadoc)
 	 * @see de.shashwat.eclipse.rxjava.sample.service.IStudentService#getStudents()
 	 */
 	@Override
 	public Single<List<StudentModel>> getStudents() {
-		// TODO Auto-generated method stub
-		return null;
+		return Single.create(e -> {
+			try {
+				TimeUnit.SECONDS.sleep(4);
+			} catch (Exception ex) {
+				e.onError(ex);
+			}
+			e.onSuccess(this.students);
+		});
 	}
 
 	/* (non-Javadoc)
@@ -24,8 +60,21 @@ public class StudentServiceImpl implements IStudentService {
 	 */
 	@Override
 	public Maybe<StudentModel> getStudent(String id) {
-		// TODO Auto-generated method stub
-		return null;
+		Optional<StudentModel> optionalStudent = this.students.stream().filter(s -> s.getId().equals(id)).findAny();
+		return Maybe.create(e -> {
+			Job.create("Getting Student with id " + id, runnable -> {
+				try {
+					TimeUnit.SECONDS.sleep(2);
+				} catch (InterruptedException ex) {
+					e.onError(ex);
+				}
+				if (optionalStudent.isPresent()) {
+					e.onSuccess(optionalStudent.get());
+				} else {
+					e.onComplete();
+				}
+			});
+		});
 	}
 
 }
