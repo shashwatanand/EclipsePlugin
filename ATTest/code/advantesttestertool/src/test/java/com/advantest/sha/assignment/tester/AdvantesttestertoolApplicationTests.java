f1b2af6405b1efc860ee583cb60e3d02b721c6cf
shashwatanand <shashwat.anand@gmail.com>
2019-03-05 20:42:04 +0100

    initial commit

---
+++ ATTest/code/advantesttestertool/src/test/java/com/advantest/sha/assignment/tester/AdvantesttestertoolApplicationTests.java
@@ -0,0 +1,16 @@
+package com.advantest.sha.assignment.tester;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AdvantesttestertoolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
