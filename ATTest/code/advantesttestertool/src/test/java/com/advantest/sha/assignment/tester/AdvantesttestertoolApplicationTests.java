e94c9fb5e31e3dbbd004c49067791343d8c947d1
shashwatanand <shashwat.anand@gmail.com>
2019-03-09 19:19:35 +0100

    Update

--- ATTest/code/advantesttestertool/src/test/java/com/advantest/sha/assignment/tester/AdvantesttestertoolApplicationTests.java
+++ ATTest/code/advantesttestertool/src/test/java/com/advantest/sha/assignment/tester/AdvantesttestertoolApplicationTests.java
@@ -1,11 +1,8 @@
 package com.advantest.sha.assignment.tester;
 
-import static org.junit.Assert.assertTrue;
-
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
-import java.util.Map;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -14,7 +11,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import com.advantest.sha.assignment.tester.data.TestSuites;
 import com.advantest.sha.assignment.tester.data.TestSystems;
 import com.advantest.sha.assignment.tester.dbmodel.TestSuiteModel;
 import com.advantest.sha.assignment.tester.dbmodel.TestSystemModel;
@@ -24,7 +20,6 @@ import com.advantest.sha.assignment.tester.utils.TesterUtil;
 @SpringBootTest
 public class AdvantesttestertoolApplicationTests {
 	
-	private static Map<String, TestSystemModel> avaibleTestSystems;
 
 	/**
 	 * Setup.
@@ -33,15 +28,6 @@ public class AdvantesttestertoolApplicationTests {
 	@BeforeClass
 	public static void setup() throws IOException {
 		TestSystems.getInstance().createTextSystems();
-		avaibleTestSystems = TestSystems.getInstance().getInMemTestSystem();
-		
-		/*
-		 * File folderRes = new ClassPathResource("testsuites").getFile(); if
-		 * (folderRes.isDirectory()) { String[] fileNames = folderRes.list(); for
-		 * (String fileName : fileNames) { TestSuiteModel testSuiteModel =
-		 * TesterUtil.getTestSuiteModel(fileName);
-		 * avaiableTestSuites.put(testSuiteModel.getName(), testSuiteModel); } }
-		 */
 	}
 
 	@Test
@@ -53,7 +39,6 @@ public class AdvantesttestertoolApplicationTests {
 				TestSuiteModel testSuite = TesterUtil.getTestSuiteModel(fileName);
 				List<TestSystemModel> foundTestSystem = TestSystems.getInstance().findAvailableTestSystem(testSuite);
 				System.out.println(foundTestSystem.size() + " test system found for " + fileName);
-			    //assertTrue(foundTestSystem.isEmpty());
 			}
 		}
 	}
